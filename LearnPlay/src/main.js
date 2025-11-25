6e6d1d077f6d59816f06696425bf5dfd84b97e0c
belal588 <belal.gadban@e.braude.ac.il>
2025-11-25 12:03:53 +0200

    goto homepage

--- LearnPlay/src/main.js
+++ LearnPlay/src/main.js
@@ -1,21 +1,23 @@
 import './style.css'
 import javascriptLogo from './javascript.svg'
-import { setupCounter } from './counter.js'
+//import { setupCounter } from './counter.js'
 
-document.querySelector('#app').innerHTML = `
-  <div>
-    <a href="../LearnPlay/src/login.html" class="block text-center w-full p-3 mt-4 bg-gray-200 text-gray-800 rounded hover:bg-gray-300">LogIn</a>
-  </div>
-`
-/*updatessssss*/
-/*second updateeeee*/
-/*updatessssss*/
-/*updatessssss*/
-
-
-/*aaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaa*/
-/*third update*/
-//after exit
-//ישאוו
-//after exit 
-//last check for commit by elias
+function goToPage(target) 
+{
+  switch (target) {
+    case "login":
+          window.location.href = "login.html";
+        break;
+    case 2:
+        
+        break;
+    case 3: 
+        
+        break; 
+    case 4:
+        
+        break;
+    default:
+        console.log("page not found");
+  }
+}
